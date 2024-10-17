ec91da0b377acc205fb6e0ed77eb77944811da46
wonjun <wonjun@kaist.ac.kr>
2024-10-17 18:00:32 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,98 @@
+#%%
+import os
+import cv2
+import argparse
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+from PIL import Image, ImageFilter
+
+import torch
+import torchvision.transforms as T
+
+from vit import vit_small, CLSHead, DINOHead, SimpleWrapper
+
+labels = ('nodule',)
+ind_to_label = {i:label for i,label in enumerate(labels)}
+label_to_ind = {v:k for k,v in ind_to_label.items()}
+
+class GaussianBlurInference(object):
+    """
+    Apply Gaussian Blur to the PIL save.
+    """
+    def __init__(self):
+        self.radius = 0.5
+
+    def __call__(self, img):
+
+        return img.filter(
+            ImageFilter.GaussianBlur(self.radius)
+            )
+
+def load_img(img_path, img_size=(256,256), patch_size=8):
+    img_path = str(img_path)
+    img = cv2.imread(img_path, 1)
+    img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_LINEAR)
+    img = Image.fromarray(img)
+    img = T.Compose(
+        [
+            GaussianBlurInference(),
+            T.ToTensor()
+        ]
+    )(img) # ( 3, img_size[0], img_size[1] )
+    
+    # make the image divisible by patch size
+    w, h = img.shape[1]-img.shape[1]%patch_size, img.shape[2]-img.shape[2]%patch_size
+    img = img[:, :w, :h].unsqueeze(0)
+    w_featmap = img.shape[-2] // patch_size
+    h_featmap = img.shape[-1] // patch_size
+
+    return img, w_featmap, h_featmap
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint_path', default='checkpoint.pth')
+    parser.add_argument('--testset_path', default='/media/wonjun/HDD2TB/VinDr-CXR-jpgs-resized512/test')
+    return parser
+
+parser = get_args_parser()
+args = parser.parse_args("")
+
+#%%
+backbone = vit_small()
+dinohead = DINOHead()
+clshead = CLSHead()
+model = SimpleWrapper(backbone, dinohead, clshead)
+
+# state_dict = torch.load('../outputs/multicxr_pretrain_ddp_nodule/checkpoint.pth')['student']
+# state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+# state_dict = {k.replace("dino.", ""): v for k, v in state_dict.items()}
+state_dict = torch.load(args.checkpoint_path)
+
+msg = model.load_state_dict(state_dict)
+print(msg)
+model.eval()
+model = model.to('cuda')
+
+testset_path = Path(args.testset_path)
+testdf = pd.read_csv('test.csv')
+
+ids = []
+preds = []
+for i, row in tqdm(testdf.iterrows(), total=len(testdf)):
+    img_path = testset_path / f"{row['ID']}.jpg"
+    img, _, _ = load_img(img_path)
+    pred = model(img.to('cuda'))[0]
+    pred = torch.sigmoid(pred).detach().cpu().numpy()[0][0]
+    # pred = int(np.round(pred))
+    pred = 1 if pred > 0.8 else 0
+    
+    ids.append(row['ID'])
+    preds.append(pred)
+
+sample_submission = pd.DataFrame({'ID':ids, 'nodule':preds})
+sample_submission.to_csv('sample_submission.csv', index=False)
+
+
+# %%
---
+++ train.py
@@ -0,0 +1,156 @@
+#%%
+import os
+import cv2
+import sys
+import math
+import argparse
+import numpy as np
+import pandas as pd
+from PIL import Image
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+
+from vit import vit_small, CLSHead, DINOHead, SimpleWrapper
+import utils
+
+
+#%%
+class NIHCXR_Dataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 df,
+                 transforms,
+                 ):
+        self.transforms = transforms
+        self.df = df
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        img_path = row['img_path']
+        # image = cv2.imread(img_path, 1) # 1 flag is for cv2.IMREAD_COLOR
+        # image = cv2.resize(image, dsize=(256,256), interpolation=cv2.INTER_LINEAR)
+        # image = Image.fromarray(image)
+        image = Image.open(img_path).convert('RGB')
+        
+        images = self.transforms(image)
+        
+        label = torch.Tensor(row[['nodule']].values.astype(int))
+        
+        return images, label
+#%%
+def get_args_parser():
+    parser = argparse.ArgumentParser('nodule-det', add_help=False)
+    
+    parser.add_argument('--trainset_csv', default='train.csv')
+    parser.add_argument('--trainset_path', default='/media/wonjun/HDD2TB/')
+    
+    parser.add_argument('--save_path', default='checkpoint.pth')
+    
+    parser.add_argument('--batch_size_per_gpu', default=8)
+    
+    parser.add_argument('--lr', default=0.00005)
+    parser.add_argument('--min_lr', default=1e-6)
+    parser.add_argument('--epochs', default=100)
+    parser.add_argument('--warmup_epochs', default=1)
+    parser.add_argument('--weight_decay', default=0.01)
+    parser.add_argument('--weight_decay_end', default=0.01)
+    parser.add_argument('--momentum_teacher', default=0.9995)
+    return parser
+
+#%%
+def train(args):
+    df = pd.read_csv(args.trainset_csv)
+    df = df.head(50)
+    print(df['nodule'].value_counts())
+    df['img_path'] = df['img_path'].apply(lambda x: os.path.join(args.trainset_path, x))
+    transforms = T.Compose(
+        [
+            T.Resize((224,224)),
+            T.ToTensor()
+        ]
+    )
+
+    dataset = NIHCXR_Dataset(df, transforms)
+    sampler = torch.utils.data.RandomSampler(dataset)
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        sampler=sampler,
+        batch_size=args.batch_size_per_gpu,
+        num_workers=0,
+        pin_memory=True,
+        drop_last=True,
+    )
+
+    backbone = vit_small()
+    dinohead = DINOHead()
+    clshead = CLSHead()
+
+    model = SimpleWrapper(backbone, dinohead, clshead)
+    model = model.to('cuda')
+
+    bce_loss = nn.BCEWithLogitsLoss()
+    param_groups = utils.get_params_groups(model)
+    optimizer = torch.optim.AdamW(param_groups)
+    lr_schedule = utils.cosine_scheduler(
+        args.lr * (args.batch_size_per_gpu) / 16.,  # linear scaling rule
+        args.min_lr,
+        args.epochs,
+        len(data_loader),
+        warmup_epochs=args.warmup_epochs,
+    )
+    wd_schedule = utils.cosine_scheduler(
+        args.weight_decay,
+        args.weight_decay_end,
+        args.epochs,
+        len(data_loader),
+    )
+
+    for epoch in range(args.epochs):
+        
+        epoch_loss = 0
+        for it, (images, labels) in enumerate(data_loader):
+            
+            it = len(data_loader) * epoch + it  # global training iteration
+            
+            for i, param_group in enumerate(optimizer.param_groups):
+                param_group["lr"] = lr_schedule[it]
+                if i == 0:  # only the first group is regularized
+                    param_group["weight_decay"] = wd_schedule[it]
+            
+            images = images.cuda()
+            labels = labels.float().cuda()
+            
+            preds = model(images)
+            preds = torch.hstack(preds)
+            loss = bce_loss(preds, labels)
+            
+            if not math.isfinite(loss.item()):
+                print("Loss is {}, stopping training".format(loss.item()), force=True)
+                sys.exit(1)
+            
+            
+            epoch_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            # if (it+1) % 10 == 0:
+            #     print(f"Epoch {epoch}, Global iteration {it+1}, loss {loss.item()}")
+            
+        print(f"Epoch {epoch}, loss {epoch_loss/len(data_loader)}")
+
+
+    torch.save(model.state_dict(), args.save_path)
+
+#%%
+if __name__ == "__main__":
+    parser = get_args_parser()
+    args = parser.parse_args()
+    train(args)
